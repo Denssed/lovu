857bc1972449f7ef121b0fed730868dd49afcc42
JeremyRodriguez <rodriguezp.jeremy@gmail.com>
2025-05-08 07:09:25 -0500

    initial push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.lovu',
+  appName: 'lovu',
+  webDir: 'dist'
+};
+
+export default config;
